23a9869bf2f747b1fc6699f65bcb9964665659cb
monaveed <monaveed@gmail.com>
2024-11-18 20:49:25 +0000

    Created first.py

---
+++ first.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# Author: Naveed
+#Version: 1.0
+#Description: First Python script.
+
+#name = "Naveed Mohammed"
+
+name= input("enter your name: ")
+
+print("My name is", name)
+
+import random
+
+print("My lucky number is", random.randint(1,50))
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
